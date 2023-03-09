b5a7b2522bbd8b5115005c0f3789fb2d9691c071
Chu Minh Duc <duc.chuminh@axonactive.com>
2023-03-09 18:17:18 +0700

    Switch from MUI to PrimeReact. Add Login page.

---
+++ web/src/components/login/login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './login.css';
+
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = () => {
+        window.location.href = 'http://localhost:3000/home';
+    }
+
+    return (
+        <div className="login-container">
+            <div className="text-5xl sm:text-6xl text-indigo-400 mb-3">Social Network</div>
+            <form className="w-full" onSubmit={login}>
+                <span className="p-float-label">
+                    <InputText id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <label htmlFor="username">Username</label>
+                </span>
+                <span className="p-float-label">
+                    <Password id="password" className="w-full" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <label htmlFor="password">Password</label>
+                </span>
+                <Button label="Login" className="w-full mt-5" type="submit" />
+            </form>
+        </div>
+    )
+}
+
+export default Login;
--- web/src/index.js
+++ web/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ThemeProvider } from '@mui/material';
-import {theme} from './theme';
+
+/* Primefaces */
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme    
+import "primereact/resources/primereact.min.css"; //core
+import "primeicons/primeicons.css"; //icons
+import 'primeflex/primeflex.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
